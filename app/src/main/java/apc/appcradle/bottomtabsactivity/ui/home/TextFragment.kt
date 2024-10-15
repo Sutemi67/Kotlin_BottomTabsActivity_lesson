d3a995b7687d0a60f7ffa67197632086bb9d6e5d
Sergey Boykov <sutemi67@gmail.com>
2024-10-15 17:22:38 +0300

    patch

--- app/src/main/java/apc/appcradle/bottomtabsactivity/ui/home/TextFragment.kt
+++ app/src/main/java/apc/appcradle/bottomtabsactivity/ui/home/TextFragment.kt
@@ -7,11 +7,11 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import apc.appcradle.bottomtabsactivity.databinding.FragmentHomeBinding
+import apc.appcradle.bottomtabsactivity.databinding.FragmentTextBinding
 
-class HomeFragment : Fragment() {
+class TextFragment : Fragment() {
 
-    private var _binding: FragmentHomeBinding? = null
+    private var _binding: FragmentTextBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -25,7 +25,7 @@ class HomeFragment : Fragment() {
         val homeViewModel =
             ViewModelProvider(this).get(HomeViewModel::class.java)
 
-        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        _binding = FragmentTextBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
         val textView: TextView = binding.textHome
--- app/src/main/java/apc/appcradle/bottomtabsactivity/ui/notifications/HelpFragment.kt
+++ app/src/main/java/apc/appcradle/bottomtabsactivity/ui/notifications/HelpFragment.kt
@@ -7,11 +7,11 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import apc.appcradle.bottomtabsactivity.databinding.FragmentNotificationsBinding
+import apc.appcradle.bottomtabsactivity.databinding.FragmentHelpBinding
 
-class NotificationsFragment : Fragment() {
+class HelpFragment : Fragment() {
 
-    private var _binding: FragmentNotificationsBinding? = null
+    private var _binding: FragmentHelpBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -25,10 +25,10 @@ class NotificationsFragment : Fragment() {
         val notificationsViewModel =
             ViewModelProvider(this).get(NotificationsViewModel::class.java)
 
-        _binding = FragmentNotificationsBinding.inflate(inflater, container, false)
+        _binding = FragmentHelpBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textNotifications
+        val textView: TextView = binding.textHelp
         notificationsViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
